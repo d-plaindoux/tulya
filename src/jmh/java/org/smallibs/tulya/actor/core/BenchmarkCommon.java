61334d5086e15055cc9632d24aad16c324969dd6
d-plaindoux <d.plaindoux@free.fr>
2025-12-01 08:53:23 +0100

    Define JMH tests / In progress

--- src/jmh/java/org/smallibs/tulya/actor/core/BenchmarkCommon.java
+++ src/jmh/java/org/smallibs/tulya/actor/core/BenchmarkCommon.java
@@ -1,5 +1,10 @@
 package org.smallibs.tulya.actor.core;
 
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
 import org.smallibs.tulya.actor.core.impl.BehaviorImpl;
 import org.smallibs.tulya.async.Promise;
@@ -7,17 +12,17 @@ import org.smallibs.tulya.async.Promises;
 import org.smallibs.tulya.async.Solvable;
 import org.smallibs.tulya.standard.Unit;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 import static org.smallibs.tulya.actor.core.ActorAddress.Companion.address;
 
-public class BenchmarkCommon {
+public abstract class BenchmarkCommon {
 
-    static final int nbActors = 1_000_000;
-    static final int nbMessages = 1_000_000;
-
-    static List<ActorReference<Solvable<Unit>>> createActors(ActorCoordinator coordinator) {
+    static List<ActorReference<Solvable<Unit>>> createActors(ActorCoordinator coordinator, int nbActors) {
         return IntStream.range(0, nbActors).mapToObj(i -> {
             try {
                 return coordinator.<Solvable<Unit>>register(
@@ -30,7 +35,10 @@ public class BenchmarkCommon {
         }).toList();
     }
 
-    void benchmark(List<ActorReference<Solvable<Unit>>> actors, Blackhole blackhole) throws Throwable {
+    @SuppressWarnings("unchecked")
+    void benchmark(List<ActorReference<Solvable<Unit>>> actors, int nbMessages, Blackhole blackhole) throws Throwable {
+        var t0 = System.currentTimeMillis();
+
         var responses = IntStream.range(0, nbMessages).mapToObj(i ->
                 actors.get(i % actors.size()).<Unit>ask(s -> s)
         ).toArray(Promise[]::new);
--- src/jmh/java/org/smallibs/tulya/actor/core/BenchmarkVirtualTest.java
+++ src/jmh/java/org/smallibs/tulya/actor/core/BenchmarkVirtualTest.java
@@ -2,39 +2,63 @@ package org.smallibs.tulya.actor.core;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 import org.smallibs.tulya.async.Solvable;
 import org.smallibs.tulya.standard.Unit;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 public class BenchmarkVirtualTest extends BenchmarkCommon {
 
     @State(Scope.Thread)
     public static class BenchState {
-        private final ActorCoordinator coordinator;
-        private final List<ActorReference<Solvable<Unit>>> actors;
+        private final Supplier<ExecutorService> service;
+
+        private ActorCoordinator coordinator;
+        protected List<ActorReference<Solvable<Unit>>> actors;
+
+        @Param({"10000", "20000", "30000", "40000", "50000", "60000", "70000", "80000", "90000", "100000"})
+        private int nbActors;
+        @Param({"100000", "200000", "300000", "400000", "500000", "600000", "700000", "800000", "900000", "1000000"})
+        protected int nbMessages;
 
         public BenchState() {
-            this.coordinator = ActorCoordinator.Companion.build(Executors.newVirtualThreadPerTaskExecutor());
-            this.actors = createActors(coordinator);
+            this.service = Executors::newVirtualThreadPerTaskExecutor;
+        }
+
+        @Setup(Level.Iteration)
+        public void doSetup() throws IOException {
+            this.coordinator = ActorCoordinator.Companion.build(service.get());
+            this.actors = createActors(this.coordinator, this.nbActors);
+        }
+
+        @Setup(Level.Iteration)
+        public void doTearDown() throws IOException {
+            // this.coordinator.close();
         }
     }
 
     @Benchmark
+    @Fork(value = 1, warmups = 0)
     @BenchmarkMode(Mode.Throughput)
-    @Warmup(iterations = 5, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
-    @Measurement(iterations = 10, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
+    @Warmup(iterations = 2, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
+    @Measurement(iterations = 4, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
     public void benchmark(BenchState state, Blackhole blackhole) throws Throwable {
-        benchmark(state.actors, blackhole);
+        super.benchmark(state.actors, state.nbMessages, blackhole);
     }
 
 }
